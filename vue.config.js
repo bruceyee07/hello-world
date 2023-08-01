a3e30359d88e3074e3d29f613beea3521445e082
bruceyee <yehao402@163.com>
2023-08-01 15:36:35 +0800

    build: add cache group

--- vue.config.js
+++ vue.config.js
@@ -11,11 +11,41 @@ module.exports = defineConfig({
       new BundleAnalyzerPlugin()
     ],
     optimization: {
+     
       splitChunks: {
+        chunks: 'all',
+        minSize: 25600,  // 提取出的chunk的最小大小
         cacheGroups: {
           default: {
             name: 'common',
-            chunks: 'initial'
+            chunks: 'initial',
+            priority: -20,
+            reuseExistingChunk: true
+          },
+          vendors: {  // 拆分第三方库（通过npm|yarn安装的库）
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'initial',
+            priority: -10,
+            reuseExistingChunk: true
+          },
+          ant: {  //拆分指定文件
+            test: /[\\/]node_modules[\\/](ant-design-vue)[\\/]/,
+            name: 'ant',
+            chunks: 'initial',
+            priority: -8
+          },
+          moment: {
+            test: /[\\/]node_modules[\\/](moment)[\\/]/,
+            name: 'moment',
+            chunks: 'initial',
+            priority: -7
+          },
+          echarts: {
+            test: /[\\/]node_modules[\\/](echarts)[\\/]/,
+            name: 'echarts',
+            chunks: 'initial',
+            priority: -6
           }
         }
       }
